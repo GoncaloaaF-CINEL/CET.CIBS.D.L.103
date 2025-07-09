d9b6ee35c2ea0395d8ccb33b671c3ede51701339
Gonçalo <goncaloaaf@gmail.com>
2025-07-09 10:20:42 +0100

    into py

--- 01_demo1/novo File.py
+++ 01_demo1/novo File.py
@@ -5,3 +5,24 @@ print("ola mundo")
 #print("com erro" + 23)
 
 print("ola mundo2")
+
+
+"""
+
+var 
+print 
+in out
+condições (if)
+loops (for e while)
+
+funçoes 
+
+lambdas?? 
+
+tuplos
+list
+set
+dict
+
+
+"""
---
+++ 02_intro/main.py
@@ -0,0 +1,44 @@
+nome = "Gonçalo"
+ano = 2025
+peso = 70.45
+aprovado = True
+
+print(nome)
+
+nome = 1234
+
+print(nome)
+
+# cmt 1 linha
+
+"""
+
+cmt
+multi 
+linha
+
+"""
+
+data_in = "12"
+# ...
+data_in = 12
+
+
+print(((ano * ano) * (ano * ano)) * ((ano * ano) * (ano * ano)) * ((ano * ano) * (ano * ano)) * ((ano * ano) * (ano * ano)))
+
+# 79946681398258369524447459012293256819248199462890625
+# 18446744073709551615
+
+
+
+nome2:str = "Gonçalo"
+ano2:int = 2025
+peso2:float = 70.45
+aprovado2:bool = True
+
+
+print(nome2)
+nome2:str = 123
+
+print(nome2)
+
